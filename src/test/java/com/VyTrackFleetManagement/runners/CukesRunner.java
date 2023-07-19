9599b49a68644957556921f25d05213e6e1af401
AndrewSloth <123920329+AndrewSloth@users.noreply.github.com>
2023-07-18 22:20:42 -0700

    Automation for B29G35-205 Story is done

--- src/test/java/com/VyTrackFleetManagement/runners/CukesRunner.java
+++ src/test/java/com/VyTrackFleetManagement/runners/CukesRunner.java
@@ -8,12 +8,13 @@ import org.junit.runner.RunWith;
         plugin = {
                 "html:target/cucumber-reports.html",
                 "rerun:target/rerun.txt",
-                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+                "json:target/cucumber.json"
         },
         features = "src/test/resources/features",
         glue = "com/VyTrackFleetManagement/step_definitions",
         dryRun = false,
-        tags = " @wip",
+        tags = "@Andrey",
         publish = true //generating a report with public link
 )
 public class CukesRunner {}
