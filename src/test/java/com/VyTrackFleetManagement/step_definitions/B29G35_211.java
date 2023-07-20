c5c722d2c809d1e235999da519d2234bd7924381
nataliiachobu <mebel2008@yahoo.com>
2023-07-20 09:46:32 -0700

    User story 211

--- src/test/java/com/VyTrackFleetManagement/step_definitions/B29G35_211.java
+++ src/test/java/com/VyTrackFleetManagement/step_definitions/B29G35_211.java
@@ -4,50 +4,71 @@ import com.VyTrackFleetManagement.pages.LoginPage;
 import com.VyTrackFleetManagement.pages.VehiclePageNC;
 import com.VyTrackFleetManagement.utilities.BrowserUtils;
 import com.VyTrackFleetManagement.utilities.Driver;
-import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ManageFunctionsStepDefinitionsNC {
+public class B29G35_211 {
     VehiclePageNC vehiclePageNC = new VehiclePageNC();
     LoginPage loginPage = new LoginPage();
 
 
-
     @When("User goes to the Vehicle Costs option")
     public void user_goes_to_the_vehicle_costs_option() {
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
         BrowserUtils.hover(vehiclePageNC.fleetModule);
-       Actions action=new Actions(Driver.getDriver());
-       action.moveToElement(vehiclePageNC.vehicleCost).click().perform();
-        WebDriverWait wait=new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(2));
+
+        vehiclePageNC.vehicleCost.click();
+
     }
 
     @Then("User should see three columns on the Vehicle Costs page")
     public void user_should_see_three_columns_on_the_vehicle_costs_page(List<String> columnList) {
-        List<WebElement> list=new ArrayList<>();
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
+        List<WebElement> list = new ArrayList<>();
+        List<String> actualList = new ArrayList<>();
         list.add(vehiclePageNC.typeHeader);
         list.add(vehiclePageNC.totalPriceHeader);
         list.add(vehiclePageNC.dateHeader);
-        for (WebElement each :list) {
-            System.out.println(each.getText());
+        for (WebElement each : list) {
+            actualList.add(each.getText());
         }
+        Assert.assertEquals(columnList, actualList);
     }
 
     @When("Checks the first checkbox")
     public void checks_the_first_checkbox() {
-
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(vehiclePageNC.firstCheckbox));
+        vehiclePageNC.firstCheckbox.click();
     }
 
     @Then("Users should verify that all the Vehicle costs are checked")
     public void users_should_verify_that_all_the_vehicle_costs_are_checked() {
-        System.out.println("hello");
+
+        List<WebElement> types = vehiclePageNC.checkboxes;
+
+
+        if (types.isEmpty()) {
+            Assert.assertTrue("We can't locate the element", false);
+        }
+
+        for (WebElement each : types) {
+
+            if (!each.isSelected()) {
+                System.out.println(each);
+                Assert.assertTrue("It's not selected", false);
+            }
+        }
+        Assert.assertTrue(true);
     }
+
 
 }
