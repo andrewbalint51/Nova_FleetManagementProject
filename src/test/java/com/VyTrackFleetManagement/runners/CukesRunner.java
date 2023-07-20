a2ed9b75b9f5b827f840093883dfa1bc1f9b5fe5
Nonos1500 <erdeneuv@duck.com>
2023-07-20 15:37:04 -0500

    B29G35_212 is done

--- src/test/java/com/VyTrackFleetManagement/runners/CukesRunner.java
+++ src/test/java/com/VyTrackFleetManagement/runners/CukesRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/VyTrackFleetManagement/step_definitions",
         dryRun = false,
-        tags = " @B29G35-221",
+        tags = "@B29G35-221",
         publish = true //generating a report with public link
 )
 public class CukesRunner {}
