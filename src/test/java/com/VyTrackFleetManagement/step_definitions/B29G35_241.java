a4864b42d7b1e2322823fc4cbee60606da400a55
yusufyalinlar <yusufyalinlar@gmail.com>
2023-07-21 14:18:43 -0700

    Login fix

--- src/test/java/com/VyTrackFleetManagement/step_definitions/B29G35_241.java
+++ src/test/java/com/VyTrackFleetManagement/step_definitions/B29G35_241.java
@@ -1,38 +1,17 @@
 package com.VyTrackFleetManagement.step_definitions;
 
-import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class B29G35_241 {
-    @Given("the user is logged in as {string}")
-    public void the_user_is_logged_in_as(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
+
+
     @When("user selects the Accounts page under Customers dropdown")
     public void user_selects_the_accounts_page_under_customers_dropdown() {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
-    @Then("verify that there are {int} filter options")
-    public void verify_that_there_are_filter_options(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
 
-
-
-    @Given("the user is logged in as {string}")
-    public void the_user_is_logged_in_as(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @When("user selects the Accounts page under Customers dropdown")
-    public void user_selects_the_accounts_page_under_customers_dropdown() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
     @Then("verify that there are {int} filter options")
     public void verify_that_there_are_filter_options(Integer int1) {
         // Write code here that turns the phrase above into concrete actions
--- src/test/resources/features/B29G35-241.feature
+++ src/test/resources/features/B29G35-241.feature
@@ -1,13 +1,13 @@
-Feature: Default
+Feature: Users should see the 8 filter items on the Accounts page
 
-	
-	@B29G35-241
-	Scenario Outline: Users should see the 8 filter items on the Accounts page
-		Given the user is logged in as "<userType>"
-		    When user selects the Accounts page under Customers dropdown
-		    Then verify that there are 8 filter options
-		
-		    Examples:
-		      | userType      |
-		      | sales manager |
-		      | store manager |
+
+  @B29G35-241
+  Scenario Outline: Users should see the 8 filter items on the Accounts page
+    Given the user logged in as "<userType>"
+    When user selects the Accounts page under Customers dropdown
+    Then verify that there are 8 filter options
+
+    Examples:
+      | userType      |
+      | sales manager |
+      | store manager |
